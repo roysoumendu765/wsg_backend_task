f67251c259aeceeeef7b606bdab71c556c728cac
Soumendu Roy <roysoumendu765@gmail.com>
2025-12-27 04:14:12 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,13 +10,14 @@ from app.database import engine, Base
 from app import models
 from contextlib import asynccontextmanager
 
-app = FastAPI(title="Transaction Webhook Service")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
+
+app = FastAPI(title="Transaction Webhook Service", lifespan=lifespan)
 
 @app.get("/")
 async def health_check():
